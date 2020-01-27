20e3d3423313cfac47a86f5dedd60f388bada10f
Tabinda <tabinda.iqbal@wearenova.co.uk>
2020-01-27 22:33:03 +0500

    code commit

---
+++ src/components/GameArea.vue
@@ -0,0 +1,28 @@
+<template>
+    <main class="gameArea">
+        <Dropbox/>
+        <TeeterTotter/>
+    </main>
+</template>
+
+<script>
+    import Dropbox from './Dropbox'
+    import TeeterTotter from './TeeterTotter'
+    export default {
+        name: "GameArea",
+        components:{
+            Dropbox, TeeterTotter
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .gameArea {
+        width: 80%;
+        height: 100%;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        padding-top: 1rem;
+    }
+</style>
---
+++ src/components/TeeterTotter.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="teeterTotter">
+        <Swing/>
+        <Pillar/>
+    </section>
+</template>
+
+<script>
+    import Swing from './Swing'
+    import Pillar from './Pillar'
+    export default {
+        name: "TeeterTotter",
+        components: {
+            Swing, Pillar
+        }
+    }
+</script>
+
+<style scoped>
+    .teeterTotter{
+        height: 35%;
+        width: 100%;
+    }
+</style>
